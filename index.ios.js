40698c79892fb103ab639162ac1758da870d9e38
Anton Cherepanov <davetoxa@gmail.com>
2015-11-18 01:35:07 +0300

    Add icon and pre bundled version

--- index.ios.js
+++ index.ios.js
@@ -17,14 +17,7 @@ var DavetoxaReactNative = React.createClass({
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Привет Антон :)
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+          Привет :)
         </Text>
       </View>
     );
---
+++ readme.md
@@ -0,0 +1,5 @@
+### React native application for education purpose
+
+
+##### Bundle app
+``` react-native bundle --platform=ios --entry-file=index.ios.js --bundle-output=mainBundle ```

349577a486ce07691e32dc28d323e7f58709aad4
Anton Cherepanov <davetoxa@gmail.com>
2015-11-25 02:49:15 +0300

    Add Notes

---
+++ App/Components/Notes.js
@@ -0,0 +1,27 @@
+var React = require('react-native');
+
+var {
+  Text,
+  View,
+  StyleSheet
+} = React;
+
+var styles = StyleSheet.create({
+  container: {
+  },
+  text: {
+    color: 'Red'
+  }
+});
+
+class Notes extends React.Component{
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Notes will be soon </Text>
+      </View>
+    )
+  }
+};
+
+module.exports = Notes;
